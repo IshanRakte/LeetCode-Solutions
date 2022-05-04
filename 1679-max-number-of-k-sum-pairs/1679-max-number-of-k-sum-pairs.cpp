b896ffaa5ba381c7e835b1247017feb60efcf8a4
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2022-05-05 00:47:14 +0530

    Time: 163 ms (71.24%), Space: 58.2 MB (80.67%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+          sort(nums.begin(), nums.end());
+        int s = 0, e = nums.size() - 1, cnt = 0;
+        while(s < e){
+            if(nums[s] + nums[e] == k) {
+                cnt++;
+                s++;
+                e--;
+            }
+            else if(nums[s] + nums[e] > k) e--;
+            else s++;
+        }
+        return cnt;
+    }
+};
