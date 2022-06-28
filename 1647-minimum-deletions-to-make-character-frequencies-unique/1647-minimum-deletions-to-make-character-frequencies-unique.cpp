d8e638e4493726abb386ebed9425cbbe24713f51
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2022-06-29 00:29:00 +0530

    Time: 178 ms (16.03%), Space: 17.5 MB (37.46%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        unordered_map<int,int>charcnt;
+        unordered_map<int,int>uniquefreq;
+        int count=0;
+        
+        for(int i=0;i<s.size();i++){
+            charcnt[s[i]-'a']++;
+        }
+        for(auto &it : charcnt){
+            int x= it.second;
+            if(uniquefreq[x]!=0){
+                while(x>0 && uniquefreq[x]!=0){
+                    x--;
+                    count++;
+                }
+            }
+            if(x>0){
+                uniquefreq[x]++;
+            }
+        }
+        return count;
+    }
+};
