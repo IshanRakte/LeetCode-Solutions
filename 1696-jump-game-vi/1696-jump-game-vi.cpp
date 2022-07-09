023b1b0a756e3d09206361d3f999ad0e89db9513
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2022-07-10 00:49:16 +0530

    Time: 187 ms (95.52%), Space: 77.3 MB (96.95%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> dq{0};
+        for (int i = 1; i < n; ++i) {
+            nums[i] = nums[dq.front()] + nums[i];
+
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) dq.pop_back(); 
+            dq.push_back(i);
+            if (i - dq.front() >= k) dq.pop_front();
+        }
+        return nums[n - 1];
+    }
+};
