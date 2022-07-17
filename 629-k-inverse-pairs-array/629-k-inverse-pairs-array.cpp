7c4046f6777d9cf156685c93f2072de40b1cb2a0
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2022-07-18 00:05:33 +0530

    Time: 32 ms (77.56%), Space: 10.4 MB (75.64%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -1,13 +1,19 @@
+const int mod = 1e9 + 7, N = 1010;
+
 class Solution {
+    int f[N][N] = {};
 public:
-    int kInversePairs(int n, int k) {
-        
-        int dp[1001][1001] = { 1 };
-        
-        for(int i=1;i<=n;++i)
-            for(int K=0; K<=k;++K)
-                for(int j=0; j<= min(K,i-1);++j)
-                    dp[i][K] = (dp[i][K] + dp[i-1][K-j])%1000000007;
-        return dp[n][k];
+    int kInversePairs(int n, int K) {
+        f[0][0] = 1;
+        for (int i = 1; i <= n; ++ i) {
+            long long s = 0; 
+            for (int j = 0; j <= K; ++ j) {
+                s += f[i - 1][j];
+                if (j >= i)
+                    s -= f[i - 1][j - i];
+                f[i][j] = ((long long)f[i][j] + s) % mod; 
+            }
+        }
+        return f[n][K];
     }
 };
