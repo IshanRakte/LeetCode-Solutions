d0eb18d425f929d895a8cd52dc6466dcf811964c
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2022-07-25 22:56:17 +0530

    Time: 6 ms (25.00%), Space: 6 MB (75.00%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
-    char repeatedCharacter(string s) {
-       unordered_map<char,int> m;
-        char ans;
-        for(int i=0;i<s.size();i++){
-            if(m.find(s[i])!=m.end()){
-                ans= s[i];
-                break;
-            }
-              m[s[i]]++;
-        }
-         return ans;
+char repeatedCharacter(string s) {
+    int seen[128] = {}, i = 0;
+    for (i = 0; i < s.size() && ++seen[s[i]] < 2; ++i);
+    return s[i];
     }
 };
