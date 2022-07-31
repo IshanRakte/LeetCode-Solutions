c5506ee74077c88058b28349d83a6175d4552982
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2022-07-31 23:59:20 +0530

    Time: 209 ms (84.36%), Space: 57.2 MB (98.68%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        
+    int sub[26] = {0}, temp[26] = {0};
+	vector<string> ans;
+
+	for (const string & s: words2) {
+		for (int i = 0; i < s.size(); i++)
+			temp[s[i] - 'a']++;
+		for (int j = 0; j < 26; j++)
+			sub[j] = max(sub[j], temp[j]);
+		memset(temp, 0, 104);
+	}
+	for (const string & s: words1) {
+		copy(begin(sub), end(sub), begin(temp));
+		for (int i = 0; i < s.size(); i++)
+			temp[s[i] - 'a']--;
+		int k = 0;
+		while (k < 26) if (temp[k++] > 0) break;
+		if (k == 26) ans.push_back(s);
+	}
+	return ans;
+    }
+};
