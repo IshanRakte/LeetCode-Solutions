f8061d1f258cede132747d2b337b43218cf37191
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2022-08-13 20:55:59 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (82.04%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,30 @@
 class Solution {
-public:
-    const vector<string> pad = {
-        "", "", "abc", "def", "ghi", "jkl",
-        "mno", "pqrs", "tuv", "wxyz"
-    };
-    
-    vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {};
-		vector<string> result;
-        result.push_back("");
+private:
+    void solve(string digits, string output, int index, vector<string>& ans, string mapping[]){
         
-        for(auto digit: digits) {
-            vector<string> tmp;
-            for(auto candidate: pad[digit - '0']) {
-                for(auto s: result) {
-                    tmp.push_back(s + candidate);
-                }
-            }
-            result.swap(tmp);
+        if(index >= digits.length()){
+            ans.push_back(output);
+            return;
         }
-        return result;
+        int num = digits[index] - '0';
+        string value = mapping[num];
+        
+        for(int i = 0; i < value.length(); i++){
+            output.push_back(value[i]);
+            solve(digits, output, index+1, ans, mapping);
+            output.pop_back();
+        }
+    }
+    
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.length() == 0)
+            return ans;
+        string output;
+        int index = 0;
+        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        solve(digits, output, index, ans, mapping);
+        return ans;
     }
 };
