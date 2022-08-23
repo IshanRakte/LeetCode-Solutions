8d5c87395ae166aca0291e26e5e4c6a31e237f14
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2022-08-23 23:18:26 +0530

    Time: 2 ms (60.92%), Space: 5.9 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -4,7 +4,7 @@ bool isPowerOfFour(int n) {
     
     if(n==0) return false;
     
-    while (1){
+    while(1){
         if(n==1 || n==4) return true;
         if(n%4!=0) return false;
         n=n/4;
