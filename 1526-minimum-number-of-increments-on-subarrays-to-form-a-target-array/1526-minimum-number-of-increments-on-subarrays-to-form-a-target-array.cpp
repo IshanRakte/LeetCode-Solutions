82d570cd07485d0cb47b86eec2b9ab090d1b0819
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2023-08-11 22:46:46 +0530

    Time: 94 ms (90.78%), Space: 73.1 MB (43.02%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -4,7 +4,7 @@ public:
         int ans = target[0];
         for(int i = 1; i < target.size(); i++){
             if(target[i] > target[i-1]){
-                ans += target[i]-target[i-1];
+                ans += target[i] - target[i-1];
             }
         }
         return ans;
