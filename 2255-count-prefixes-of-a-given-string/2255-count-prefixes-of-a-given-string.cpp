d00cafb562bd0dfc07dfb1cbec148bc764edc87d
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2023-08-20 21:24:47 +0530

    Time: 6 ms (84.80%), Space: 11.9 MB (39.77%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int countPrefixes(vector<string>& words, string s) {
-    int count=0;
-    for(int i=0; i<words.size(); i++){
-        string z= s.substr(0, words[i].size());
-        if(words[i] == z){
-            count++;
-        }    
-    }
-    return count;
+        int count = 0;
+        for(int i=0; i<words.size(); i++){
+            if(s.find(words[i]) == 0) { 
+                count++;
+            }
+        }
+        return count;
     }
 };
