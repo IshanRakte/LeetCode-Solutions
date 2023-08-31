ef85d3f51778e38a4166c72d6d59093203752e24
Ishan Rakte <78254476+IshanRakte@users.noreply.github.com>
2023-09-01 01:28:34 +0530

    Time: 167 ms (96.09%), Space: 164.3 MB (65.79%) - LeetHub

---
+++ 2771-longest-non-decreasing-subarray-from-two-arrays/2771-longest-non-decreasing-subarray-from-two-arrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxNonDecreasingLength(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        vector<int> dp1(n, 1), dp2(n, 1);
+        int ans = 1;
+        
+        for(int i = 1; i < n; i++){
+            if(nums1[i-1] <= nums1[i]) dp1[i] = max(dp1[i],1+dp1[i-1]);
+            if(nums2[i-1] <= nums1[i]) dp1[i] = max(dp1[i],1+dp2[i-1]);
+            
+            if(nums2[i-1] <= nums2[i]) dp2[i] = max(dp2[i],1+dp2[i-1]);
+            if(nums1[i-1] <= nums2[i]) dp2[i] = max(dp2[i],1+dp1[i-1]);
+            
+            ans = max({ans, dp1[i], dp2[i]}); 
+        }
+        return ans;
+    }
+};
